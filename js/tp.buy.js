a8efb308b08f7d2a9bd86adc33db317852619513
Nien Chen <chennien1990@chennien.com>
2024-10-26 10:03:06 +0800

    + TapPay KYC
    For first-time TapPay subscriptions, Taiwanese users must provide a mobile number for identity verification when binding their card. https://www.tappaysdk.com/taiwan-zhtw/about/news/118

--- js/tp.buy.js
+++ js/tp.buy.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.jQuery || alert("Please disable Adblock & clean caches.\n請停用 Adblock及清除快取") || window.open("//goo.gl/o8y9R1", "_blank");
+window.jQuery || alert("Please disable Adblock & clean caches.\n請停用 Adblock及清除快取") || window.open("//bit.ly/3I7cEJz", "_blank");
 
 // 語系檔
 var _h$ = {tp: [
@@ -12,8 +12,10 @@ var _h$ = {tp: [
 		item: "Item", 
 		plan: "Plan", 
 		amt: "Amount", 
+		ask: "Encountering payment issues?", 
 		card: ["Credit Card Payment", "Credit Card Subscription"], 
-		orgs: "VISA, MasterCard, JCB & Union Pay", 
+		phone: ["Country (A-Z)", "Mobile number, Card Holder"], 
+		types: "VISA, MasterCard, JCB & Union Pay", 
 		pay: "Checkout", 
 		agree: [
 			"I hereby authorize and permit to sign the power of attorney to execute the credit card payment by Hearty Journal.", 
@@ -26,11 +28,9 @@ var _h$ = {tp: [
 		// JS
 		ok: "OK", 
 		no: "Cancel", 
-		ask: [
-			"Encountering payment issues?", 
-			"If you can’t complete the payment, your credit card might not support 3D Secure Authentication (SMS OTP). Please disable it for this transaction and try again.", 
-			"Disable 3D Secure", 
-			"3D Secure disabled. Please continue with the payment process."
+		kyc: [
+			"To comply with financial regulator's anti-fraud rules in Taiwan, please provide the phone number on file with your card-issuing bank for ID verification.", 
+			"The phone number $no doesn't match the bank's records. Please provide the correct number and try again. Thank you."
 		], 
 		done: [
 			"Payment completed. Thank you for upgrading to the VIP plan of Hearty Journal.", 
@@ -54,8 +54,10 @@ var _h$ = {tp: [
 		item: "方案", 
 		plan: "內容", 
 		amt: "金額", 
+		ask: "遇到刷卡問題？", 
 		card: ["信用卡付款", "信用卡訂閱"],  
-		orgs: "VISA、MasterCard、JCB、銀聯卡", 
+		phone: ["國碼 (A-Z)", "持卡人手機號碼"], 
+		types: "VISA、MasterCard、JCB、銀聯卡", 
 		pay: "結帳", 
 		agree: [
 			"我同意自下次交易起由溫度日記向 TapPay發動信用卡授權指示", 
@@ -68,11 +70,9 @@ var _h$ = {tp: [
 		// JS
 		ok: "好的", 
 		no: "取消", 
-		ask: [
-			"遇到刷卡問題？", 
-			"如您無法順利刷卡，可能由於信用卡不支援 3D簡訊驗證，請於本次交易中暫停驗證", 
-			"暫停簡訊驗證", 
-			"簡訊驗證已暫停，請您繼續付款流程"
+		kyc: [
+			"為遵守台灣金管會的防詐新制，請您提供您在發卡銀行留存的手機號碼，以便進行交易簡訊認證。", 
+			"您提供的手機號碼 $no 與銀行留存的不符。請提供正確的號碼並再試一次。"
 		], 
 		done: [
 			"付款完成，謝謝您購買溫度日記 VIP", 
@@ -96,8 +96,10 @@ var _h$ = {tp: [
 		item: "方案", 
 		plan: "内容", 
 		amt: "金额", 
+		ask: "遇到刷卡问题？", 
 		card: ["银行卡付款", "银行卡订阅"],  
-		orgs: "VISA、MasterCard、JCB、银联卡", 
+		phone: ["国码 (A-Z)", "持卡人手机号"], 
+		types: "VISA、MasterCard、JCB、银联卡", 
 		pay: "结帐", 
 		agree: [
 			"我同意自下次交易起由温度日记向 TapPay发动银行卡授权指示", 
@@ -110,11 +112,9 @@ var _h$ = {tp: [
 		// JS
 		ok: "好的", 
 		no: "取消", 
-		ask: [
-			"遇到刷卡问题？", 
-			"如您无法顺利刷卡，可能由于银行卡不支援 3D简讯验证，请于本次交易中暂停验证", 
-			"暂停简讯验证", 
-			"简讯验证已暂停，请您继续支付流程"
+		kyc: [
+			"为遵守台湾金管会的防诈新制，请您提供您在发卡银行留存的手机号码，以便进行交易简讯认证。", 
+			"您提供的手机号 $no 与银行留存的不符。请提供正确的号码并再试一次。"
 		], 
 		fail: [
 			"交易失败，请确认输入数据无误，或更换银行卡，或另选支付方式，如问题持续请联繫客服", // 储值
@@ -139,9 +139,9 @@ $(function(){
 		// 非手機 APP，才啟用 GP
 		if(!tp_isapp()) tp_init_gpay(); // GP
 		tp_init_apay(); // AP
-		np_init_alipay(); // Alipay
+		np_init_alipay(); // 支付寶
 
-		$("#agree").parent().remove(); // 移除續扣同意說明
+		$(".mobile").add($("#agree").parent()).remove(); // 移除手機號碼+續扣同意
 	}
 	// 訂閱
 	else{
@@ -180,7 +180,7 @@ function tp_params(get){
 		}
 
 		let _3ds = getUrlPara("3ds");
-		if(getcookie("hearty_3ds")=="0") _3ds = 0; // 關 3D
+		// if(getcookie("hearty_3ds")=="0") _3ds = 0; // 關 3D
 
 		// 將網址參數寫入 DOM以隱藏
 		$b.attr({
@@ -191,7 +191,8 @@ function tp_params(get){
 			"data-cc": getUrlPara("cc")
 		});
 
-		if(typeof history.replaceState=="function") history.replaceState(null, "", location.pathname);
+		// 非手機 APP，才藏網址
+		if(!tp_isapp() && typeof history.replaceState=="function") history.replaceState(null, "", location.pathname);
 	}
 	// get
 	else{
@@ -200,9 +201,26 @@ function tp_params(get){
 }
 
 function tp_init(){
+	let dev_mode = $("body").attr("data-prod")==0, 
+		$d = $(".demo");
+
+	// TapPay
 	TPDirect.setupSDK(146938, "app_tqcNPY9hA34zrTxrpSiJ263QjhRojqktIxruWM47YobDh83m7E1GKlf3XPN4", 
-		$("body").attr("data-prod")==0 ? "sandbox" : "production"
+		dev_mode ? "sandbox" : "production"
 	);
+
+	// 測試卡號備註欄
+	if(dev_mode){
+		$d.removeClass("hidden").on("click", function(){
+			window.open("//docs.tappaysdk.com/tutorial/zh/reference.html#test-card", "_blank");
+		}).find("a").on("click", function(e){
+			e.stopPropagation();
+			navigator.clipboard.writeText($(this).text());
+		});
+	}
+	else{
+		$d.remove();
+	}
 }
 
 function tp_dpay_btn(on){
@@ -229,29 +247,42 @@ function tp_init_dpay(){
 			}
 		});
 
+		// 預填電話國碼 (如尚未驗證)
+		let $cc = $("#phone_cc");
+		if($cc.length>0 && !$cc.val()){
+			let cc = dial_code(getUrlPara("cc")||"");
+			$cc.val(cc).find("[value="+cc+"]").attr({selected: "selected"});
+		}
+
 		// 付款鈕
 		let $btn = $("#dpay_btn").on("click", function(e){
 			e.preventDefault();
 			tp_dpay_btn(false);
 			TPDirect.card.getPrime(function(r){
-				tp_pay(1, r.card.prime||"");
+				let c = r.card;
+				tp_pay(1, c.prime||"", c.countrycode||"");
 			});
 		});
 
-		TPDirect.card.onUpdate(function(r){
-			$btn.prop({disabled: !r.canGetPrime});
-		});
+			TPDirect.card.onUpdate(function(r){
+				$btn.prop({disabled: !r.canGetPrime});
+			});
 
 		// 授權同意
 		$("#agree").on("click", function(e){
 			e.preventDefault();
-			msg('<i class="fal fa-badge-check"></i> '+_h("tp-agree-1"));
+			tp_msg('<i class="fal fa-badge-check"></i> '+_h("tp-agree-1"));
 		});
 	}
 	catch(e){
 		console.error("Dpay Error");
 	}
 }
+	function dial_code(cc){
+		cc = (cc||"").toUpperCase();
+		let c = {AD:376,AE:971,AF:93,AG:1,AI:1,AL:355,AM:374,AO:244,AQ:672,AR:54,AS:1,AT:43,AU:61,AW:297,AX:358,AZ:994,BA:387,BB:1,BD:880,BE:32,BF:226,BG:359,BH:973,BI:257,BJ:229,BL:590,BM:1,BN:673,BO:591,BQ:599,BR:55,BS:1,BT:975,BV:47,BW:267,BY:375,BZ:501,CA:1,CC:61,CD:243,CF:236,CG:242,CH:41,CI:225,CK:682,CL:56,CM:237,CN:86,CO:57,CR:506,CU:53,CV:238,CW:599,CX:61,CY:357,CZ:420,DE:49,DJ:253,DK:45,DM:1,DO:1,DZ:213,EC:593,EE:372,EG:20,EH:212,ER:291,ES:34,ET:251,FI:358,FJ:679,FK:500,FM:691,FO:298,FR:33,GA:241,GB:44,GD:1,GE:995,GF:594,GG:44,GH:233,GI:350,GL:299,GM:220,GN:224,GP:590,GQ:240,GR:30,GS:500,GT:502,GU:1,GW:245,GY:592,HK:852,HM:672,HN:504,HR:385,HT:509,HU:36,ID:62,IE:353,IL:972,IM:44,IN:91,IO:246,IQ:964,IR:98,IS:354,IT:39,JE:44,JM:1,JO:962,JP:81,KE:254,KG:996,KH:855,KI:686,KM:269,KN:1,KP:850,KR:82,KW:965,KY:1,KZ:7,LA:856,LB:961,LC:1,LI:423,LK:94,LR:231,LS:266,LT:370,LU:352,LV:371,LY:218,MA:212,MC:377,MD:373,ME:382,MF:590,MG:261,MH:692,MK:389,ML:223,MM:95,MN:976,MO:853,MP:1,MQ:596,MR:222,MS:1,MT:356,MU:230,MV:960,MW:265,MX:52,MY:60,MZ:258,NA:264,NC:687,NE:227,NF:672,NG:234,NI:505,NL:31,NO:47,NP:977,NR:674,NU:683,NZ:64,OM:968,PA:507,PE:51,PF:689,PG:675,PH:63,PK:92,PL:48,PM:508,PN:870,PR:1,PS:970,PT:351,PW:680,PY:595,QA:974,RE:262,RO:40,RS:381,RU:7,RW:250,SA:966,SB:677,SC:248,SD:249,SE:46,SG:65,SH:290,SI:386,SJ:47,SK:421,SL:232,SM:378,SN:221,SO:252,SR:597,SS:211,ST:239,SV:503,SX:1,SY:963,SZ:268,TC:1,TD:235,TF:262,TG:228,TH:66,TJ:992,TK:690,TL:670,TM:993,TN:216,TO:676,TR:90,TT:1,TV:688,TW:886,TZ:255,UA:380,UG:256,UM:246,US:1,UY:598,UZ:998,VA:379,VC:1,VE:58,VG:1,VI:1,VN:84,VU:678,WF:681,WS:685,YE:967,YT:262,ZA:27,ZM:260,ZW:263};
+		return cc in c ? c[cc] : 886;
+	}
 
 function tp_init_apay(){
 	try{
@@ -360,19 +391,50 @@ function tp_init_gpay(){
 }
 
 // Pay-by-Prime
-function tp_pay(payvia, prime){
+function tp_pay(payvia, prime, card_country){ // eg. card_country = TW
 	// payvia: 1 信用卡; 8 AP; 9 GP
 	if(prime.length>0){
+		let data = {
+			payvia: payvia, 
+			prime: prime
+		};
+
+		// cardholder_verify (僅 Direct Pay)
+		if(payvia==1){
+			data["card_country"] = card_country||"";
+
+			let $phone_no = $("#phone_no");
+			if($phone_no.length>0){
+				let phone_cc = $("#phone_cc").val()||"", 
+					phone_no = $phone_no.val()||"";
+
+				if(!!phone_cc && phone_no.length>3){
+					// 去掉前導 0
+					// 台灣手機 09
+					if(phone_cc==886) phone_no = phone_no.replace(/^0(?=9)/, "");
+
+					// 加國碼
+					data["phone"] = "+"+phone_cc+phone_no;
+				}
+				// 要求補填手機
+				else{
+					tp_kyc();
+					tp_dpay_btn(true);
+					return;
+				}
+			}
+		}
+
+		// ### debug
+		console.log("* fn tp_pay: "+JSON.stringify(data));
+
 		$.ajax({
 			type: "post", 
 			url: location.origin+"/shop/tp.prime?"+tp_params(true), 
 			async: true, 
 			crossDomain: true, 
 			dataType: "json", 
-			data: {
-				payvia: payvia, 
-				prime: prime
-			}, 
+			data: data, 
 			timeout: 15000
 		}).then(function(r){
 			let v = r["Values"]||"";
@@ -408,14 +470,14 @@ function tp_pay(payvia, prime){
 					}
 					// 交易完成 (信用卡非 3DS / AP / GP)
 					else{
-						msg('<i class="fal fa-check-circle"></i> '+_h("tp-done-0")+'<br><i class="fal fa-file"></i> '+_h("tp-done-1", {$no: orderno||"*"}));
+						tp_msg('<i class="fal fa-check-circle"></i> '+_h("tp-done-0")+'<br><i class="fal fa-file"></i> '+_h("tp-done-1", {$no: orderno||"*"}));
 						top.location.href = location.origin+"/shop/tp.thankyou?o="+orderno;
 					}
 				break;
 
 				// 2 交易失敗
 				case 2:
-					tp_pay_report("2", JSON.stringify(v||""));
+					tp_pay_report(2, JSON.stringify(v||""));
 
 					let $b = $("body"), 
 						recurring = $b.attr("data-recurring")==1;
@@ -433,9 +495,18 @@ function tp_pay(payvia, prime){
 					tp_dpay_btn(true);
 				break;
 
+				// 4 KYC 手機驗證失敗
+				case 4:
+					tp_pay_report(4, JSON.stringify(v||""));
+
+					tp_kyc( "+"+($("#phone_cc").val()||"")+($("#phone_no").val()||"") );
+
+					tp_dpay_btn(true);
+				break;
+
 				// 3 訂單格式不符
 				case 3:
-					tp_pay_err("3", v["err"]||"");
+					tp_pay_err(3, v["err"]||"");
 				break;
 
 				// D 其他錯誤
@@ -450,16 +521,16 @@ function tp_pay(payvia, prime){
 	}
 }
 	// 錯誤顯示
-	function tp_pay_err(no, msg){
+	function tp_pay_err(no, txt){
 		no = no||"";
-		msg = msg||"";
-		tp_pay_report(no, msg);
+		txt = txt||"";
+		tp_pay_report(no, txt);
 
-		msg('<i class="far fa-sad-tear"></i> ['+no+"] "+_h("tp-err")+"<br>"+msg);
+		tp_msg('<i class="far fa-sad-tear"></i> ['+no+"] "+_h("tp-err")+"<br>"+txt);
 		tp_dpay_btn(true);
 	}
 		// 錯誤回傳
-		function tp_pay_report(no, msg){
+		function tp_pay_report(no, txt){
 			// forms.gle/AgcyYpiwkvhqj9WR9
 			// docs.google.com/forms/d/1C6ex-5D2znrAEF4PSTBKv1oSp8MvuB5LZf_jAVFkuuU/edit#responses
 			fetch("//docs.google.com/forms/d/e/1FAIpQLSe15z1D2VmTe7W_F4uRjth7xSRT8rNUxyKG9qrcVRVRXM9nMA/formResponse", {
@@ -468,7 +539,7 @@ function tp_pay(payvia, prime){
 				body: new URLSearchParams({
 					"entry.553484449": $("[data-hid]").text()||"", // Hearty ID
 					"entry.729548178": no, 
-					"entry.20989936": msg, 
+					"entry.20989936": txt, 
 					"entry.792614445": navigator.userAgent||"", 
 					"entry.2000689905": new Date((new Date).getTime()+288e5).toISOString().split("T")[0]
 				})
@@ -482,23 +553,6 @@ function np_init_alipay(){
 		top.location.href = location.origin+"/shop/np.buy?alipay=1&"+tp_params(true);
 		// top.location.href = location.origin+"/robots.txt?"+tp_params(true)+"&alipay=1";
 	}).show();
-}
-
-/* fn ask 未使用 */
-function ask(){
-	let $b = $("body");
-	if($b.attr("data-3ds")==1){
-		alertify.set({labels: {ok: '<i class="fas fa-toggle-on"></i> '+_h("tp-ask-2"), cancel: _h("tp-no")}, buttonReverse: false});
-		alertify.confirm('<i class="fal fa-credit-card"></i> '+_h("tp-ask-1"), function(e){
-			if(e){
-				$b.attr("data-3ds", 0);
-				msg('<i class="fal fa-comment-alt-slash"></i> '+_h("tp-ask-3"));
-			}
-		});
-	}
-	else{
-		tp_support();
-	}
 }
 
 // 當地匯率
@@ -544,9 +598,9 @@ function getcookie(cname){
 	return "";
 }
 
-function msg(t){
+function tp_msg(txt, callback){
 	alertify.set({labels:{ok: '<i class="fas fa-check"></i> '+_h("tp-ok")}});
-	alertify.alert(t);
+	alertify.alert(txt||"", callback||function(){});
 }
 
 function tp_loading(on){
@@ -559,12 +613,40 @@ function tp_loading(on){
 		});
 }
 
+function tp_back(){
+	tp_loading(true);
+	top.location.href = "//bitly.com/3fmoEYD";
+	// a[href] 有 bug 無法轉頁，原因不明
+}
+
 // 開新頁至客服信箱
 function tp_support(){
+	window.open("//hearty.me/d?f=2", "_blank");
+	// a[href] 有 bug 無法轉頁，原因不明
+
+	/*
 	$("<a>", {
 		target: "_blank", 
 		href: "//hearty.me/d?f=2"
 	}).get(0).click();
+	*/
+}
+
+function tp_kyc(phone){
+	let $phone_no = $("#phone_no");
+	// 驗證前提醒
+	if(!phone){
+		tp_msg('<i class="far fa-mobile"></i> '+_h("tp-kyc-0"), function(){
+			$phone_no.focus();
+		});
+	}
+	// 驗證失敗後訊息
+	else{
+		tp_msg('<i class="far fa-info-circle"></i> '+_h("tp-kyc-1", {$no: phone||""}), 
+			function(){
+			$phone_no.val("").focus();
+		});
+	}
 }
 
 function tp_lang(){
